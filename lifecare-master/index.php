dd3d03578c7a62ae0940a3a57dd916e110f13558
Muhammad <mhammadadeel11@gmail.com>
2025-05-05 00:14:20 +0500

    extantion change into .php

--- lifecare-master/index.php
+++ lifecare-master/index.php
@@ -39,7 +39,7 @@
       <header>
          <div class="header-top wow fadeIn">
             <div class="container">
-               <a class="navbar-brand" href="index.html"><img src="images/logo.png" alt="image"></a>
+               <a class="navbar-brand" href="index.php"><img src="images/logo.png" alt="image"></a>
                <div class="right-header">
                   <div class="header-info">
                      <div class="info-inner">
@@ -67,7 +67,7 @@
 				  
                   <div id="navbar" class="navbar-collapse collapse">
                      <ul class="nav navbar-nav">
-                        <li><a class="active" href="index.html">Home</a></li>
+                        <li><a class="active" href="index.php">Home</a></li>
                         <li><a data-scroll href="#about">About us</a></li>
                         <li><a data-scroll href="#service">Services</a></li>
                         <li><a data-scroll href="#doctors">Doctors</a></li>
@@ -277,7 +277,7 @@
                   <div class="appointment-form">
                      <h3><span>+</span> Book Appointment</h3>
                      <div class="form">
-                        <form action="index.html">
+                        <form action="index.php">
                            <fieldset>
                               <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
                                  <div class="row">
