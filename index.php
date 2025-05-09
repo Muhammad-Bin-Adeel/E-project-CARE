03dba1f059148c244273c1f88d834e22b93a772c
Muhammad <mhammadadeel11@gmail.com>
2025-05-10 00:14:13 +0500

    ...

--- index.php
+++ index.php
@@ -1,20 +1,9 @@
 <!DOCTYPE html>
 <html lang="en">
-<style>
-     /* Position submenus properly */
-     .dropdown-submenu {
-      position: relative;
-    }
-    .dropdown-submenu .dropdown-menu {
-      top: 0;
-      left: 100%;
-      margin-top: -1px;
-    }
-</style>
 
 <head>
     <meta charset="utf-8">
-    <title>care - Hospital Website Template</title>
+    <title>MEDINOVA - Hospital Website Template</title>
     <meta content="width=device-width, initial-scale=1.0" name="viewport">
     <meta content="Free HTML Templates" name="keywords">
     <meta content="Free HTML Templates" name="description">
@@ -77,48 +66,32 @@
     </div>
     <!-- Topbar End -->
 
-<!-- Navbar Start -->
-<div class="container-fluid sticky-top bg-white shadow-sm mb-5">
+
+    <!-- Navbar Start -->
+    <div class="container-fluid sticky-top bg-white shadow-sm">
         <div class="container">
             <nav class="navbar navbar-expand-lg bg-white navbar-light py-3 py-lg-0">
                 <a href="index.php" class="navbar-brand">
-                    <h1 class="m-0 text-uppercase text-primary"><i class="fa fa-clinic-medical me-2"></i>care</h1>
+                    <h1 class="m-0 text-uppercase text-primary"><i class="fa fa-clinic-medical me-2"></i>Medinova</h1>
                 </a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
                     <span class="navbar-toggler-icon"></span>
                 </button>
                 <div class="collapse navbar-collapse" id="navbarCollapse">
                     <div class="navbar-nav ms-auto py-0">
-                        <a href="index.php" class="nav-item nav-link">Home</a>
+                        <a href="index.php" class="nav-item nav-link active">Home</a>
                         <a href="about.php" class="nav-item nav-link">About</a>
-                   
-
-                       <div class="nav-item dropdown">
-                            <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">Service</a>
-                            <!-- <a class="dropdown-item dropdown-toggle" href="#">Find doctor by speciality</a> -->
+                        <a href="service.php" class="nav-item nav-link">Service</a>
+                        <a href="#" class="nav-item nav-link">Pricing</a>
+                        <div class="nav-item dropdown">
+                            <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">Pages</a>
                             <div class="dropdown-menu m-0">
-
-                                 <li class="dropdown-submenu">
-                             <a class="dropdown-item dropdown-toggle" href="#">Dermatologist</a>
-                             <ul class="dropdown-menu">
-                             <a class="dropdown-item dropdown-toggle" href="#">Dermatologist in lahore </a>
-                             </ul>
-                             </li>
-
-                                  <li class="dropdown-submenu">
-                             <a class="dropdown-item dropdown-toggle" href="#">Gynecologistt</a>
-                             <ul class="dropdown-menu">
-                             <li><a class="dropdown-item" href="#">Gynecologist in karachi </a></li>
-                             </ul>
-                             </li>
-
-                                <li class="dropdown-submenu">
-                             <a class="dropdown-item dropdown-toggle" href="#">Urologist</a>
-                             <ul class="dropdown-menu">
-                             <li><a class="dropdown-item" href="#">Urologistin Islamabad </a></li>
-                             </ul>
-                             </li>
-
+                                <a href="blog.php" class="dropdown-item">Blog Grid</a>
+                                <a href="#" class="dropdown-item">Blog Detail</a>
+                                <a href="#" class="dropdown-item">The Team</a>
+                                <a href="#" class="dropdown-item">Testimonial</a>
+                                <a href="appointment.php" class="dropdown-item">Appointment</a>
+                                <a href="search.php" class="dropdown-item">Search</a>
                             </div>
                         </div>
                         <a href="contact.php" class="nav-item nav-link">Contact</a>
@@ -135,11 +108,11 @@
         <div class="container py-5">
             <div class="row justify-content-start">
                 <div class="col-lg-8 text-center text-lg-start">
-                    <h5 class="d-inline-block text-primary text-uppercase border-bottom border-5" style="border-color: rgba(256, 256, 256, .3) !important;">Welcome To care</h5>
+                    <h5 class="d-inline-block text-primary text-uppercase border-bottom border-5" style="border-color: rgba(256, 256, 256, .3) !important;">Welcome To Medinova</h5>
                     <h1 class="display-1 text-white mb-md-4">Best Healthcare Solution In Your City</h1>
                     <div class="pt-2">
                         <a href="" class="btn btn-light rounded-pill py-md-3 px-md-5 mx-2">Find Doctor</a>
-                        <a href="appointment.php" class="btn btn-outline-light rounded-pill py-md-3 px-md-5 mx-2">Appointment</a>
+                        <a href="" class="btn btn-outline-light rounded-pill py-md-3 px-md-5 mx-2">Appointment</a>
                     </div>
                 </div>
             </div>
@@ -297,7 +270,7 @@
                 </div>
                 <div class="col-lg-6">
                     <div class="bg-white text-center rounded p-5">
-                        <h1 class="mb-4">Book An Appointment</h1>
+                    <h1 class="mb-4"><a href="appointment.php">Book An Appointment</a></h1>
                         <form>
                             <div class="row g-3">
                                 <div class="col-12 col-sm-6">
@@ -506,14 +479,14 @@
     <!-- Team End -->
 
 
-    <!-- Search Start -->
+   <!-- Search Start -->
     <div class="container-fluid bg-primary my-5 py-5">
         <div class="container py-5">
             <div class="text-center mx-auto mb-5" style="max-width: 500px;">
                 <h5 class="d-inline-block text-white text-uppercase border-bottom border-5">Find A Doctor</h5>
                 <h1 class="display-4 mb-4">Find A Healthcare Professionals</h1>
                 <h5 class="text-white fw-normal">Duo ipsum erat stet dolor sea ut nonumy tempor. Tempor duo lorem eos sit sed ipsum takimata ipsum sit est. Ipsum ea voluptua ipsum sit justo</h5>
-            </div>
+            </div >
             <div class="mx-auto" style="width: 100%; max-width: 600px;">
                 <div class="input-group">
                     <select class="form-select border-primary w-25" style="height: 60px;">
@@ -742,18 +715,6 @@
 
     <!-- Template Javascript -->
     <script src="js/main.js"></script>
-    <script>
-  document.querySelectorAll('.dropdown-submenu .dropdown-toggle').forEach(function (el) {
-    el.addEventListener('click', function (e) {
-      e.preventDefault();
-      e.stopPropagation();
-      let nextEl = this.nextElementSibling;
-      if (nextEl && nextEl.classList.contains('dropdown-menu')) {
-        nextEl.classList.toggle('show');
-      }
-    });
-  });
-</script>
 </body>
 
 </html>
